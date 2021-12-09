682fbb934bb34647b630e9159725fd622470712b
WhimsicalCodeBin <95654521+WhimsicalCodeBin@users.noreply.github.com>
2021-12-09 00:21:00 -0600

    Update EssenceDrainContagion.cs

--- EssenceDrainContagion.cs
+++ EssenceDrainContagion.cs
@@ -108,14 +108,12 @@ namespace EssenceDrainContagion
             var position = GameController.Game.IngameState.Camera.WorldToScreen(_currentTarget.Item2.Pos);
             Input.SetCursorPos(position);
             //Input.Update();
-            /*
+            
             if (_currentTarget.Item2.HasBuff("contagion", false)) yield return Input.Keypress(Settings.ContagionKey.Value);
             if (_currentTarget.Item2.HasBuff("contagion", true) && _currentTarget.Item2.HasBuff("essence_drain", false)) yield return Input.Keypress(Settings.EssenceDrainKey.Value);
             if (_currentTarget.Item2.HasBuff("contagion", true) && _currentTarget.Item2.HasBuff("essence_drain", true)) yield return Input.Keypress(Settings.BlightKey.Value);
-            */
-            if (Input.MousePosition == position) {
-                yield return Input.KeyPress(_currentTarget.Item2.HasBuff("contagion", true) ? Settings.EssenceDrainKey.Value : Settings.ContagionKey.Value);
-            }
+            
+            //yield return Input.KeyPress(_currentTarget.Item2.HasBuff("contagion", true) ? Settings.EssenceDrainKey.Value : Settings.ContagionKey.Value);
         }
 
         private bool ValidTarget(Entity entity)
