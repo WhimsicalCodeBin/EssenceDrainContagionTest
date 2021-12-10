c4dcfa6bf3304748a0e76cb1828cf28f3bfde601
WhimsicalCodeBin <95654521+WhimsicalCodeBin@users.noreply.github.com>
2021-12-10 01:06:21 -0600

    Update EssenceDrainContagion.cs

--- EssenceDrainContagion.cs
+++ EssenceDrainContagion.cs
@@ -174,7 +174,7 @@ namespace EssenceDrainContagion
             Input.MouseMove();
             System.Threading.Thread.Sleep(10);
             
-            if (_currentTarget.Item2.HasBuff("contagion", false)) yield return Input.Keypress(Settings.ContagionKey.Value);
+            if (_currentTarget.Item2.HasBuff("contagion", false)) Input.Keypress(Settings.ContagionKey.Value);
             //yield return Input.KeyPress(_currentTarget.Item2.HasBuff("contagion", true) ? Settings.EssenceDrainKey.Value : Settings.ContagionKey.Value);
             //Full Rotation
             //
