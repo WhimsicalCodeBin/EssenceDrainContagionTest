07566b5179f0aaa33f98c0c2f9d15b7931cb694d
WhimsicalCodeBin <95654521+WhimsicalCodeBin@users.noreply.github.com>
2021-12-11 19:15:25 -0600

    Update EssenceDrainContagion.cs

--- EssenceDrainContagion.cs
+++ EssenceDrainContagion.cs
@@ -178,7 +178,7 @@ namespace EssenceDrainContagion
 
             //LogBuffs(_currentTarget.Item2);
 
-            if (!_currentTarget.Item2.HasBuff("contagion", true)) yield return Input.KeyPress(Settings.ContagionKey.Value);
+            if (_currentTarget.Item2.HasBuff("contagion", false)) yield return Input.KeyPress(Settings.ContagionKey.Value);
             else if (_currentTarget.Item2.HasBuff("contagion", true) && !_currentTarget.Item2.HasBuff("siphon_damage", true)) yield return Input.KeyPress(Settings.EssenceDrainKey.Value);
             else if (_currentTarget.Item2.HasBuff("contagion", true) && _currentTarget.Item2.HasBuff("siphon_damage", true)) yield return Input.KeyPress(Settings.BlightKey.Value); 
             //else yield return Input.KeyPress(Settings.ContagionKey.Value);
